d9c923f0125ccd0a58d6dcb3299c26e9888713a9
Eugene Hauptmann <eugene@reactivelions.com>
2024-12-18 03:47:18 -0500

    wip

--- src/kernels/mod.rs
+++ src/kernels/mod.rs
@@ -15,4 +15,5 @@ pub trait AutodiffBackend: Backend + burn::tensor::backend::AutodiffBackend {
     // fn euclidean_pairwise_distance(x: Tensor<Self, 2>) -> Tensor<Self, 1>;
 }
 
+// this line along with the `backward` module is what's needed to enable support for a particular device below
 impl AutodiffBackend for Autodiff<JitBackend<WgpuRuntime, f32, i32>> {}
