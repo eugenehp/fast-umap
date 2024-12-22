eebfbb3575bffc933df5f1cfe61b01fa9b729029
Eugene Hauptmann <eugene@reactivelions.com>
2024-12-22 12:40:08 -0500

    backward kenel compiles and runs, but shape is off

--- src/kernels/forward.rs
+++ src/kernels/forward.rs
@@ -1,5 +1,5 @@
 use super::{kernel::*, Backend};
-use burn::tensor::{ops::FloatTensor, Shape};
+use burn::tensor::{ops::FloatTensor, Shape, Tensor};
 use burn_jit::{
     kernel::into_contiguous, tensor::JitTensor, FloatElement, IntElement, JitBackend, JitRuntime,
 };
@@ -35,5 +35,43 @@ impl<R: JitRuntime, F: FloatElement, I: IntElement> Backend for JitBackend<R, F,
         );
 
         output
+    }
+
+    fn euclidean_pairwise_distance_backward(
+        x: FloatTensor<Self>,
+        grad_x: FloatTensor<Self>,
+        output: FloatTensor<Self>,
+    ) -> FloatTensor<Self> {
+        println!("backend - euclidean_pairwise_distance_backward");
+        let x = into_contiguous(x);
+        let n = x.shape.dims[0];
+
+        let output_shape = Shape::from(vec![n, n]);
+        let buffer = x
+            .client
+            .empty(output_shape.num_elements() * std::mem::size_of::<F>());
+        let grad_output: JitTensor<R, F> =
+            JitTensor::new_contiguous(x.client.clone(), x.device.clone(), output_shape, buffer);
+
+        // Launch the Euclidean pairwise distance kernel
+        let cube_dim = CubeDim { x: 16, y: 16, z: 1 }; // Example cube size
+        let cubes_needed_in_x = (n as f32 / cube_dim.x as f32).ceil() as u32;
+        let cubes_needed_in_y = (n as f32 / cube_dim.y as f32).ceil() as u32;
+        let cube_count = CubeCount::Static(cubes_needed_in_x, cubes_needed_in_y, 1);
+
+        // Launch the kernel
+        unsafe {
+            euclidean_pairwise_distance_backward_kernel::launch_unchecked::<F, R>(
+                &x.client,
+                cube_count,
+                cube_dim,
+                x.as_tensor_arg(1),
+                output.as_tensor_arg(1),
+                grad_output.as_tensor_arg(1),
+                grad_x.as_tensor_arg(1),
+            );
+        }
+
+        grad_x
     }
 }
